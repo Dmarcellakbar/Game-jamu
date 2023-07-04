fbf69294e5a952a76b6a6bee518d087fd6bd5c05
Pepe <muhammadrickyperdanaputra@gmail.com>
2023-07-04 15:28:34 +0700

    update 2

---
+++ Assets/Script/Menu/Button_Setting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Setting : MonoBehaviour
+{
+    public void OnMenuButton ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Script/Menu/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton ()
+    {
+        SceneManager.LoadScene("Play");
+    }
+    public void OnSettingButton ()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+    public void OnQuitButton ()
+    {
+        Application.Quit();
+    }
+    public void OnMenuButton ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
